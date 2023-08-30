350511e82823df93a6ae42d8f5bfe26924e34e7d
Aditya Ranjan <ranjan.aditya2501@gmail.com>
2023-08-30 17:26:15 +0530

    Add Design File

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a64fdceadbe0195201f877982c50bb253645f38531d6e560bff9b6256e147523
+size 1518
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab873648826225fd4493a7151a14389f56c159eefa8a4708f06b562e36d42a05
+size 1643
